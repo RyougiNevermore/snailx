9bca6669e8a6f5e142874a6c3e48d69fa2922d2d
RyougiNevermore <ryougi.nevermore@hotmail.com>
2019-08-13 18:57:35 +0800

    update log

--- log.go
+++ log.go
@@ -64,36 +64,47 @@ func (s *slog) Debugf(format string, args ...interface{}) {
 	loc := getRunLocation(2)
 	s.locker.Lock()
 	defer s.locker.Unlock()
+	_, _ = s.writer.Write([]byte(fmt.Sprintf("[snailx] [\x1b[32mDEBUG\x1b[0m] [%-29s] %s %s" , time.Now().Format("2006-01-02 15:04:05.999999999"), loc, fmt.Sprintf(format, args...))))
 
-
-	fmt.Println("[snailx] [\x1b[32mDEBUG\x1b[0m]", fmt.Sprintf("[%-29s]", time.Now().Format("2006-01-02 15:04:05.999999999")), getFileLine(2), fmt.Sprintf(format, args...))
 }
 
 func (s *slog) Infof(format string, args ...interface{}) {
 	if s.Level > LoggerLevelInfo {
 		return
 	}
-	fmt.Println("[snailx] [\x1b[36mINFO\x1b[0m ]", fmt.Sprintf("[%-29s]", time.Now().Format("2006-01-02 15:04:05.999999999")), getFileLine(2), fmt.Sprintf(format, args...))
+	loc := getRunLocation(2)
+	s.locker.Lock()
+	defer s.locker.Unlock()
+	_, _ = s.writer.Write([]byte(fmt.Sprintf("[snailx] [\x1b[36mINFO\x1b[0m ] [%-29s] %s %s" , time.Now().Format("2006-01-02 15:04:05.999999999"), loc, fmt.Sprintf(format, args...))))
 }
 
 func (s *slog) Warnf(format string, args ...interface{}) {
 	if s.Level > LoggerLevelWarn {
 		return
 	}
-	fmt.Println("[snailx] [\x1b[33mWARN\x1b[0m ]", fmt.Sprintf("[%-29s]", time.Now().Format("2006-01-02 15:04:05.999999999")), getFileLine(2), fmt.Sprintf(format, args...))
+	loc := getRunLocation(2)
+	s.locker.Lock()
+	defer s.locker.Unlock()
+	_, _ = s.writer.Write([]byte(fmt.Sprintf("[snailx] [\x1b[33mWARN\x1b[0m ] [%-29s] %s %s" , time.Now().Format("2006-01-02 15:04:05.999999999"), loc, fmt.Sprintf(format, args...))))
 }
 
 func (s *slog) Errorf(format string, args ...interface{}) {
 	if s.Level > LoggerLevelError {
 		return
 	}
-	fmt.Println("[snailx] [\x1b[31mERROR\x1b[0m]", fmt.Sprintf("[%-29s]", time.Now().Format("2006-01-02 15:04:05.999999999")), getFileLine(2), fmt.Sprintf(format, args...))
+	loc := getRunLocation(2)
+	s.locker.Lock()
+	defer s.locker.Unlock()
+	_, _ = s.writer.Write([]byte(fmt.Sprintf("[snailx] [\x1b[31mERROR\x1b[0m] [%-29s] %s %s" , time.Now().Format("2006-01-02 15:04:05.999999999"), loc, fmt.Sprintf(format, args...))))
 }
 
 func (s *slog) Fatalf(format string, args ...interface{}) {
 	if s.Level > LoggerLevelFatal {
 		return
 	}
-	fmt.Println("[snailx] [\x1b[31mFATAL\x1b[0m]", fmt.Sprintf("[%-29s]", time.Now().Format("2006-01-02 15:04:05.999999999")), getFileLine(2), fmt.Sprintf(format, args...))
+	loc := getRunLocation(2)
+	s.locker.Lock()
+	defer s.locker.Unlock()
+	_, _ = s.writer.Write([]byte(fmt.Sprintf("[snailx] [\x1b[31mFATAL\x1b[0m] [%-29s] %s %s" , time.Now().Format("2006-01-02 15:04:05.999999999"), loc, fmt.Sprintf(format, args...))))
 	os.Exit(2)
 }
